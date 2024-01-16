8e38e3cef043713148854e92610fc2f55a5fb1bb
please <please@xiaotata.local>
2024-01-16 13:28:54 +0800

    feat: card

--- src/card/index.js
+++ src/card/index.js
@@ -2,17 +2,23 @@
  * @Author: please
  * @Date: 2024-01-03 11:23:22
  * @LastEditors: please
- * @LastEditTime: 2024-01-15 17:06:53
+ * @LastEditTime: 2024-01-16 11:52:56
  * @Description: 请填写简介
  */
 //Component Object
 Component({
+  externalClasses: ['custom-class'],
   options: {
+    // multipleSlots: true
   },
   properties: {
-    customClass: {
-      type: String,
-      value: ''
+    useSlot: {
+      type: Boolean,
+      value: false
+    },
+    useInlineDescSlot: {
+      type: Boolean,
+      value: false
     },
     width: {
       type: Number,
@@ -21,6 +27,18 @@ Component({
     radius: {
       type: Number,
       value: 20
+    },
+    title: {
+      type: String,
+      value: ''
+    },
+    value: {
+      type: String,
+      value: ''
+    },
+    inlineDesc: {
+      type: String,
+      value: ''
     }
   }
 })
